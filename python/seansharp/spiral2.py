51d6bd52e2ff8c23894dd39118b0320f3e05ec6a
Mervyn_CJR <56317821+MuffinC@users.noreply.github.com>
2022-05-30 17:38:13 +0800

    sr
    storage

---
+++ python/seansharp/spiral2.py
@@ -0,0 +1,8 @@
+def generateMatrix(n):
+    res, lo = [[n * n]], n * n
+    while lo > 1:
+        lo, hi = lo - len(res), lo
+        # print('res:', res)
+        res = [[i for i in range(lo, hi)]] + [list(j) for j in zip(*res[::-1])]
+    return res
+print(generateMatrix(3))
