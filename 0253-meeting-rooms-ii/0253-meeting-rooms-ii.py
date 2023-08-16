d3ba8e1390af0913f472498f3132f004631a6ed6
Mervyn_CJR <56317821+MuffinC@users.noreply.github.com>
2023-08-16 21:04:52 +0800

    Time: 78 ms (91.15%), Space: 20 MB (50.09%) - LeetHub

---
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        rooms=0
+        if not intervals:return 0
+        endp=0
+
+        starters=sorted([x[0]for x in intervals])
+        ends=sorted([x[1]for x in intervals])
+
+        for i in range(len(starters)):
+            if starters[i]>=ends[endp]:
+                endp+=1
+            else:
+                rooms+=1
+        
+        return rooms
